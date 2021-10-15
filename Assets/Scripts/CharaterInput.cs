dfa8d9c85d02ff671c81f59231703ac024451bb9
eungyukm <86348187+eungyukm@users.noreply.github.com>
2021-10-15 22:46:04 +0900

    캐릭터 인풋 클래스 생성

---
+++ Assets/Scripts/CharaterInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaterInput : MonoBehaviour
+{
+    public string moveAxisName = "Vertical";
+    public string rotateAxisName = "Horizontal";
+
+    public float move
+    {
+        get;
+        private set;
+    }
+
+    public float rotate
+    {
+        get;
+        private set;
+    }
+
+    private void Update()
+    {
+        move = Input.GetAxis(moveAxisName);
+        rotate = Input.GetAxis(rotateAxisName);
+
+        Debug.Log("move : " + move);
+        Debug.Log("rotate : " + rotate);
+    }
+}
