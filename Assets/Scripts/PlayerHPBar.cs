f4089b90d29b496b01192867fce41362662dcf03
eungyukm <86348187+eungyukm@users.noreply.github.com>
2021-10-16 02:37:27 +0900

    UI 완료

---
+++ Assets/Scripts/PlayerHPBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPBar : MonoBehaviour
+{
+    public Slider hpBar;
+
+    public void Damage()
+    {
+        hpBar.value -= 0.1f;
+    }
+
+    public void Heal()
+    {
+        hpBar.value += 0.1f;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpBar.value = 1;
+    }
+}
