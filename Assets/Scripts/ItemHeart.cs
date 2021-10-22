68d6423ebc4ff4daca9d79aa934a5158b4cb7527
eungyukm <eungyukm@gmail.com>
2021-10-23 06:31:25 +0900

    좀비 게임 Chapter4

---
+++ Assets/Scripts/ItemHeart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHeart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<CharacterManager>().hp += 0.1f;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnManger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManger : MonoBehaviour
+{
+    public GameObject zombie;
+    public GameObject heart;
+    public Transform[] spawnArray;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnZombie());
+        StartCoroutine(SpawnHeart());
+    }
+
+    /// <summary>
+    /// 좀비를 생성하는 코루틴
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator SpawnZombie()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+
+            Instantiate(zombie, spawnArray[Random.Range(0, 3)]);
+        }
+    }
+
+    IEnumerator SpawnHeart()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+
+            Instantiate(heart, spawnArray[Random.Range(3, 6)]);
+        }
+    }
+}
